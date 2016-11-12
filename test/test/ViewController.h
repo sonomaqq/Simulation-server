6a31b39242cabdbb18064f65dba0b4d94f1627bf
陈俊峰 <cjfchenjunfeng@163.com>
2016-11-12 17:21:36 +0800

    创建项目

---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by chenjunfeng on 2016/11/12.
+//  Copyright © 2016年 chenjunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by chenjunfeng on 2016/11/12.
+//  Copyright © 2016年 chenjunfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
